74812d92934a2cd1019a6119391f7d1f16315366
Joakim Wånggren <joakim.wanggren@schibsted.com>
2017-01-20 23:32:44 +0100

    Added links to all examples

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
         <title>SPiD PHP Examples</title>
     </head>
     <body>
-        Hello
+        <ul>
+        <?php
+            $dirs = (glob('./*', GLOB_ONLYDIR));
+            foreach ($dirs as $dir) {
+                if (in_array($dir, ['./config', './vendor'])) { continue; }
+                echo "<li><a href='{$dir}'>". ltrim($dir, './') . "</a></li>";
+            }
+        ?>
+        </ul>
     </body>
 </html>
